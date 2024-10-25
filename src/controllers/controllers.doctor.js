39e1507175438a64cfe17e813f40b5f759227537
Amos Barbato <amosbarbato@gmail.com>
2024-10-25 20:11:34 -0300

    fix: fix request error

--- src/controllers/controllers.doctor.js
+++ src/controllers/controllers.doctor.js
@@ -2,37 +2,37 @@ import serviceDoctor from "../services/service.doctor.js"
 
 async function toList(req, res) {
   const name = req.query.name;
-  const doctors = await serviceDoctor.toList(name);
 
+  const doctors = await serviceDoctor.toList(name);
   res.status(200).json(doctors);
 }
 
 async function toInsert(req, res) {
   const { name, specialty, icon } = req.body;
-  const doctor = await serviceDoctor.toInsert(name, specialty, icon);
 
+  const doctor = await serviceDoctor.toInsert(name, specialty, icon);
   res.status(201).json(doctor);
 }
 
 async function toEdit(req, res) {
   const id_doctor = req.params.id_doctor;
   const { name, specialty, icon } = req.body;
-  const doctor = await serviceDoctor.toEdit(id_doctor, name, specialty, icon);
 
+  const doctor = await serviceDoctor.toEdit(id_doctor, name, specialty, icon);
   res.status(200).json(doctor);
 }
 
 async function toDelete(req, res) {
-  const id_doctor = req.query.id_doctor;
-  const doctor = await serviceDoctor.toDelete(id_doctor);
+  const id_doctor = req.params.id_doctor;
 
+  const doctor = await serviceDoctor.toDelete(id_doctor);
   res.status(200).json(doctor);
 }
 
 async function toListServices(req, res) {
-  const id_doctor = req.query.id_doctor;
-  const serv = await serviceDoctor.toListServices(id_doctor);
+  const id_doctor = req.params.id_doctor;
 
+  const serv = await serviceDoctor.toListServices(id_doctor);
   res.status(200).json(serv);
 }
 
