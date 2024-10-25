1e90e89ec37ca5b6b07f59a78aaad19203e7dc5b
Amos Barbato <amosbarbato@gmail.com>
2024-10-25 20:13:48 -0300

    feat: insert token validation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,24 @@
 import { Router } from "express";
 import controllerDoctor from "../controllers/controllers.doctor.js";
 import controllersUser from "../controllers/controllers.user.js";
+import jwt from "../utils/token.js";
 
 const router = Router();
 
 // doctors
-router.get("/doctors", controllerDoctor.toList);
-router.post("/doctors", controllerDoctor.toInsert);
-router.put("/doctors/:id_doctor", controllerDoctor.toEdit);
-router.delete("/doctors/:id_doctor", controllerDoctor.toDelete);
-router.get("/doctors/:id_doctor/services", controllerDoctor.toListServices);
+router.get("/doctors", jwt.ValidateToken, controllerDoctor.toList);
+router.post("/doctors", jwt.ValidateToken, controllerDoctor.toInsert);
+router.put("/doctors/:id_doctor", jwt.ValidateToken, controllerDoctor.toEdit);
+router.delete(
+  "/doctors/:id_doctor",
+  jwt.ValidateToken,
+  controllerDoctor.toDelete
+);
+router.get(
+  "/doctors/:id_doctor/services",
+  jwt.ValidateToken,
+  controllerDoctor.toListServices
+);
 
 // users
 router.post("/users/register", controllersUser.toInsert);
